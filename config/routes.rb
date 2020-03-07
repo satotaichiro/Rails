56dc3e3295fa08397f83487ddf9005639c6cd4ed
satotaichiro <tapoya09@gmail.com>
2020-03-07 19:08:05 +0900

    finish[3-7]

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,17 @@
 Rails.application.routes.draw do
+      # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+      get 'todolists/new'
 
-	get 'top' => 'homes#top'
+      get 'top' => 'homes#top'
+      
+      post 'todolists' => 'todolists#create'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
 
+     get 'todolists' => 'todolists#index'
+
+     get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+     get 'todolists/:id/edit' => 'todolists#edit' , as: 'edit_todolist'
+
+     patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
+ end
